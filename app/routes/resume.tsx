9516a811f4481c03e3b6d7787e62301c40b925fc
Shahdin Salman <125382694+shahdinsalman23@users.noreply.github.com>
2025-09-01 15:02:18 +0500

    updated

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -75,7 +75,7 @@ const resume = () => {
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
-                            <ATS score={feedback.ATS.score || 0} suggestionds={feedback.ATS.tips || []} />
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
                             <Details feedback={feedback} />
                         </div>
                     ) : (
